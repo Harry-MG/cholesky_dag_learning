2505c4360fb073a26531d09f176578327b2acc52
Harry-MG <hmg42@cam.ac.uk>
2022-03-25 07:36:53 +0000

    updated noisy_version

--- noisy_version.py
+++ noisy_version.py
@@ -3,22 +3,23 @@ import numpy as np
 
 from utils import random_dag
 
-n = 10
+n = 5
 spar = .75
 U = random_dag(n, spar)  # generates a random upper triangular matrix A
 rand_perm = np.random.permutation(n)
 P = np.eye(n)
 P[list(range(n))] = P[list(rand_perm)]
 dag = P @ U @ np.transpose(P)  # now A represents a DAG not necessarily in topological order
-noise_cov = ...
+noise_cov = np.diag(np.random.rand(n))  # diagonal as in the setup in Uhler paper
 
 
 def sample_inverse_covariance(dag, noise_cov, N):
     n = np.shape(dag)[0]
     data = np.zeros((n, N))
     for i in range(N):
-        noise = ...
+        noise = np.random.multivariate_normal(mean=np.zeros(n), cov=noise_cov)
         X = np.linalg.inv(np.eye(n) - dag) @ noise
         data[:, i] = X
     covmat = np.cov(data)
     invcov = np.linalg.inv(covmat)
+    return invcov
--- non-binary_version.py
+++ non-binary_version.py
@@ -1,1 +1,23 @@
 # sketch of non-binary (dag non-binary) version
+import numpy as np
+
+from methods import dag_from_ltr
+
+
+def random_nb_dag(dim, sparsity):
+    # generates random  non-binary upper triangular matrix with given upper triangular sparsity, representing the
+    # weighted adjacency matrix of a DAG
+
+    A = np.random.rand(dim, dim)  # might want to range the matrix entries in [.2, .8] for example
+
+    zero_indices = np.random.choice(np.arange(A.size), replace=False,
+                                    size=int(A.size * sparsity))
+
+    A[np.unravel_index(zero_indices, A.shape)] = 0
+
+    A = np.transpose(np.tril(A))
+
+    A = A - np.diag(np.diag(A))
+
+    return A
+
