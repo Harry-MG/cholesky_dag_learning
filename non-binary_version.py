9928f01963793ec6407a7f8b71e09c31f46ecfa0
Hmgitlearning <67194164+Hmgitlearning@users.noreply.github.com>
2022-04-04 16:26:03 +0100

    updated non-binary_version.py with non-exact pivots for the non-binary case

--- non-binary_version.py
+++ non-binary_version.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 from methods import dag_from_ltr
-from utils import random_dag
+from utils import random_dag, remove_duplicate_matrices
 
 
 def random_nb_dag(dim, sparsity):
@@ -45,7 +45,7 @@ def dfs_search(A):
         for pair in current:
 
             # get the indices of the rows that are in position i or greater and that have 1 on the diagonal
-            eligible_rows = [j for j in range(i, n) if pair[0][j, j] == 1]
+            eligible_rows = [j for j in range(i, n) if np.abs(pair[0][j, j] - 1) < 1e-4]
 
             # only proceed when B has at least one eligible row
             if eligible_rows:
@@ -131,19 +131,20 @@ def ltr_search(invcov):
     initial_children = [j for j in range(n) if invcov[j, j] == 1]
     # initialise node instance
     node = Node(matrix=invcov, children=initial_children, parent=None, permutation=np.eye(n))
-    while depth < n-1 and np.sum(np.diag(node.matrix)) > n:  # use > n because the diag elements are > 1 unless...
-        print(node.matrix)
-        print('depth = ' + str(depth))
-        print('children = ' + str(node.children))
-        print(node.matrix[depth, depth])
+    while depth < n - 1:  # and np.sum(np.diag(node.matrix)) > n + 1e-4:  # use > n because the diag elements are > 1
+        # unless...
+        # print(node.matrix)
+        # print('depth = ' + str(depth))
+        # print('children = ' + str(node.children))
+        # print(node.matrix[depth, depth])
 
-        if np.abs(node.matrix[depth, depth] - 1) < 1e-4:
-            # pass to next depth
-            depth += 1
-            node.children = node.children[1:]
-            print('pass through')
+        # if np.abs(node.matrix[depth, depth] - 1) < 1e-4:
+        # pass to next depth
+        # depth += 1
+        # node.children = node.children[1:]
+        # print('pass through')
 
-        elif node.children:
+        if node.children:
             # update children to exclude first child and pass to first child, increase depth by 1
             index = node.children[0]
 
@@ -209,7 +210,72 @@ def recovered_ltr_nb_dag_count(n, N, spar):
         invcov = np.transpose(np.eye(n) - A) @ (np.eye(n) - A)
         eligible_mat = dag_from_ltr(invcov)
 
-        if (eligible_mat == A).all():
+        if ((eligible_mat - A) < 1e-3).all():
             count += 1
 
     return 'successfully recovered ' + str(count) + ' out of ' + str(N) + ' non-binary DAGs'
+
+
+def nb_dags_from_dfs(invcov):
+    # input: inverse covariance matrix
+    # output: estimated DAG adjacency matrices A such that invcov = (I-A)^T (I-A)
+    n = np.shape(invcov)[0]
+    permutations = dfs_search(invcov)
+    estimates = [np.eye(n) - perm.T @ np.linalg.cholesky(perm @ invcov @ perm.T).T @ perm for perm in permutations]
+
+    return close_to_avg(estimates)
+
+
+def recovered_dfs_nb_dag_count(n, N, spar):
+    # counts the number of permutation matrices that dags_from_dfs successfully recovers
+    # arguments:
+    # n: dimension of the DAGs considered (number of nodes)
+    # N: number of samples
+    # spar: sparsity of the upper triangular part of the upper triangular DAG adjacency matrix
+    count = 0
+    ambiguous_count = 0
+    for i in range(N):
+        U = random_nb_dag(n, spar)  # generates a random upper triangular matrix A
+        rand_perm = np.random.permutation(n)
+        P = np.eye(n)
+        P[list(range(n))] = P[list(rand_perm)]
+        A = P @ U @ np.transpose(P)  # now A represents a DAG not necessarily in topological order
+        invcov = np.transpose(np.eye(n) - A) @ (np.eye(n) - A)
+        eligible_mats = nb_dags_from_dfs(invcov)
+
+        if len(eligible_mats) > 1:
+            ambiguous_count += 1
+
+        else:
+            if (eligible_mats[0] == A).all():
+                count += 1
+
+    return 'successfully recovered ' + str(count) + ' out of ' + str(N) + ' DAGs and ' + str(
+        ambiguous_count) + ' were ambiguous'
+
+
+def close_to_avg(list_of_matrices, tol=1e-4):
+    avg_mat = sum(list_of_matrices) / len(list_of_matrices)
+    diffs = [np.linalg.norm(mat - avg_mat) for mat in list_of_matrices]
+    if max(diffs) < tol:
+        return avg_mat
+    else:
+        return 'distinct DAGs returned'
+
+
+n = 5
+spar = .7
+U = random_nb_dag(n, spar)  # generates a random upper triangular matrix A
+rand_perm = np.random.permutation(n)
+P = np.eye(n)
+P[list(range(n))] = P[list(rand_perm)]
+A = P @ U @ np.transpose(P)  # now A represents a DAG not necessarily in topological order
+print('true DAG')
+print(A)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+invcov = np.transpose(np.eye(n) - A) @ (np.eye(n) - A)
+mat = nb_dags_from_dfs(invcov)
+print(mat)
+print(((mat - A) < 1e-4).all())
+print('---------------------------------------------')
+
